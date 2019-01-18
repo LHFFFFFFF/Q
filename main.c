f0cf10c416bd1d0a09e837b46c19c004a0fd8720
Your Name <you@example.com>
2019-01-18 10:10:34 +0800

    项目初始化

--- main.c
+++ main.c
@@ -1,8 +1,60 @@
 #include <stdio.h>
 
+struct student
+{
+    int yuwen;
+    int shuxue;
+    int yingyu;
+    char name[100];
+};
+
+
 int main(){
 
-    printf("hello world!");
+    printf("xueshengchengjiguanlixitong\n");
+
+
+    while(1){
+        printf("1---tianjiaxueshengchengji\n");
+        printf("2---shanchuxueshengchengji\n");
+        printf("3---xianshisuoyouxueshengchengji\n");
+        printf("4---yewui1:suoyouzongfenbujigedexuesheng(xiaoyu180)\n");
+        printf("5---yewu2:meiyikedoubujigedexuesheng\n");
+        printf("6---yewu3:xianshizongfenzuigaodexuesheng\n");
+        printf("7---yewu4:xianshipingjunfenzuigao,qiemeiyoubujigekemudexuesheng\n");
+        printf("8---tuichu\n");
+
+        printf("qingxuanze:\n");
+
+        int code;
+        scanf("%d",&code);
+
+        if(code == 1){
+
+        }
+        if(code == 2){
+            
+        }
+        if(code == 3){
+            
+        }
+        if(code == 4){
+            
+        }
+        if(code == 5){
+            
+        }
+        if(code == 6){
+            
+        }
+        if(code == 7){
+            
+        }
+        if(code == 8){
+            printf("yuichuxitong\n");
+            break;
+        }
+    }
 
     return 0;
 
